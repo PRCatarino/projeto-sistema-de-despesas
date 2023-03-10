614c2ce8d6309e877a7fb253f8c7c1a704b2b124
GuilhermeMaywald <guilhermemaywald@gmail.com>
2023-03-10 09:36:09 -0300

    Criando Modal de Categorias

--- src/components/services/categories.services.ts
+++ src/components/services/categories.services.ts
@@ -3,3 +3,11 @@ export async function ListCategories() {
       method:'GET',
     }).then((response) => response.json())
 }
+
+export async function CreateCategory(body) {
+  fetch("http://localhost:3000/categories",{
+    method:'POST',
+    body:JSON.stringify(body),
+    headers: {"Content-Type": "application/json"},
+  }).then((response) => response.json())
+}
